9a8f064ccfe5915bd3ed38ffc815d7bbec97254a
YufanZheng11 <yufan.zheng1@gmail.com>
2022-06-03 16:35:45 +0800

    exercise 04

---
+++ Chapter2/exercises/exercise_04.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter your age: ";
+    unsigned int age;
+    cin >> age;
+    cout << "Your age in month is " << age * 12 << endl;
+}
