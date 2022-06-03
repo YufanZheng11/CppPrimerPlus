bc93ad43fc496c882863ec230461e4d07dbed569
YufanZheng11 <yufan.zheng1@gmail.com>
2022-06-03 17:07:29 +0800

    0302

---
+++ Chapter03/02_exceed.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+#define ZERO 0
+
+int main()
+{
+    short sam = SHRT_MAX;
+    unsigned short sue = sam;
+
+    cout << "Sam has " << sam << " dollars and Sue has " << sue << " dollars deposited" << endl;
+    sam++;
+    sue++;
+    cout << "Now Sam has " << sam << " dollars and Sue has " << sue << " dollars deposited" << endl;
+    cout << "Poor Sam!" << endl;
+
+    sam = ZERO;
+    sue = ZERO;
+    cout << "Sam has " << sam << " dollars and Sue has " << sue << " dollars deposited" << endl;
+    sam--;
+    sue--;
+    cout << "Now Sam has " << sam << " dollars and Sue has " << sue << " dollars deposited" << endl;
+    cout << "Lucky Sue!" << endl;
+}
