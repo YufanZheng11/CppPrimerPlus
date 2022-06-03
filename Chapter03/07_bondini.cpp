e2e122cca44949e31214db3d6936b6c3a23e2c51
YufanZheng11 <yufan.zheng1@gmail.com>
2022-06-03 17:56:45 +0800

    0307

---
+++ Chapter03/07_bondini.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "\aOperation \"HyperHype\" is now activated!\n";
+    cout << "Enter your code: ________\b\b\b\b\b\b\b\b";
+    long code;
+    cin >> code;
+    cout << "\aYou entered " << code << "...\n";
+    cout << "\aCode verified! Proceed with Plan 23!\n";
+}
