8ea33624c3114109c30a7ab52d048019b3f160e0
YufanZheng11 <yufan.zheng1@gmail.com>
2022-06-03 16:33:52 +0800

    exercise 03

---
+++ Chapter2/exercises/exercise_03.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void PrintBlindMice();
+void PrintMiceRun();
+
+
+int main()
+{
+    PrintBlindMice();
+    PrintBlindMice();
+    PrintMiceRun();
+    PrintMiceRun();
+}
+
+void PrintBlindMice()
+{
+    cout << "Three blind mice" << endl;
+}
+
+void PrintMiceRun()
+{
+    cout << "See how they run" << endl;
+}
