e7f8f98f8f4c7dac33b06a104724ab0c8d7ae147
YufanZheng11 <yufan.zheng1@gmail.com>
2022-06-04 15:09:32 +0800

    exercise 03_06

---
+++ Chapter03/exercises/exercise_03_06.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    double miles, gallons;
+    cout << "How many miles have you driven: ";
+    cin >> miles;
+    cout << "How many gallons have gotten: ";
+    cin >> gallons;
+    cout << "Gallon / mile is: " << gallons / miles;
+}
