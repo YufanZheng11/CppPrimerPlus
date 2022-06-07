24fbd00aa27bb15e137cb5dfe3c99ac8c601631e
YufanZheng11 <yufan.zheng1@gmail.com>
2022-06-07 22:44:43 +0800

    0417

--- Chapter04/17_use_new.cpp
+++ Chapter04/17_use_new.cpp
@@ -4,11 +4,20 @@ int main()
 {
     using namespace std;
     int nights = 1001;
-    int* pt = new int;
+    auto* pt = new int;
     *pt = 1001;
 
     cout << "nights value = ";
     cout << nights << ": location " << &nights << endl;
     cout << "int ";
     cout << "value = " << *pt << ": location = " << pt << endl;
+
+    auto* pd = new double;
+    *pd = 100001.0;
+    cout << "double ";
+    cout << "value = " << *pd << ": location = " << pd << endl;
+    cout << "Location of pointer pd: " << &pd << endl;
+
+    cout << "Size of pt: " << sizeof pt << ", size of *pt: " << sizeof *pt << endl;
+    cout << "Size of pd: " << sizeof pd << ", size of *pd: " << sizeof *pd << endl;
 }
